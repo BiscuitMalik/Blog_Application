3cd2e5bb2e48a55901efd179f98e93006fba3464
Haroon Asif <haroonasifmalik123@gmail.com>
2023-09-12 00:04:49 +0500

    Storing form information in database.

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -10,13 +12,32 @@ function handler(req, res) {
             email,
             name,
             message
-        }
-        console.log(newMessage)
-        res.status(201).json({
-            message: 'Successfuly stored ', data: newMessage
-        })
+        };
 
+        let client;
+        try {
+
+            client = await MongoClient.connect('mongodb+srv://haroon:KAPhHY2eBEvAgDzT@cluster0.kvvkn7c.mongodb.net/')
+        } catch (error) {
+            res.status(500).json({ message: 'Could not connect to the base!' })
+            return;
+        }
+        const db = client.db()
+        try {
+            const result = await db.collection('message').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close()
+            res.status(500).json({ message: 'Message storing failed!' })
+            return;
+        }
+        client.close()
+
+        res.status(201).json({ message: 'Successful', data: newMessage })
     }
 
+
+
 }
+
 export default handler;
